743945eac1c0fab330b90a40bf81f32e49ec5385
Mike Childs <mike.childs@education.gov.uk>
2018-05-29 10:50:18 +0100

    Validation Errors Connection String Constructor Added

---
+++ src/ESFA.DC.Data.ILR.1819.ValidationErrors.Model/ValidationErrorsConstructor.cs
@@ -0,0 +1,10 @@
+﻿namespace ESFA.DC.Data.ILR._1819.ValdationErrors.Model
+{
+    public partial class ValidationErrors
+    {
+        public ValidationErrors(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
