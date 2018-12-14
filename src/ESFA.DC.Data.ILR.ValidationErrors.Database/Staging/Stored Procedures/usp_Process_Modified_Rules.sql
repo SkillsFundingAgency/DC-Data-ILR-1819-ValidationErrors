b825ad192cd9022350b618f26e1ef4d34953b246
Hal Angseesing <tomlingames@gmail.com>
2018-12-14 09:16:19 +0000

    Added ModifiedRules table to allow overridesw by the business (text, severity etc)

---
+++ src/ESFA.DC.Data.ILR.ValidationErrors.Database/Staging/Stored Procedures/usp_Process_Modified_Rules.sql
@@ -0,0 +1,93 @@
+﻿CREATE PROCEDURE [Staging].[usp_Process_Rules]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		
+		MERGE INTO [dbo].[Rules] AS Target
+		USING (
+				  SELECT   [Rulename]
+						,[Severity]
+						,[Message]
+				  FROM [Staging].[Rules]
+				UNION ALL 
+					SELECT   'ZIP_EMPTY' as [Rulename]
+							,'F' as [Severity]
+							,'Zip folder must contain only one XML file' as [Message]
+				UNION ALL 
+					SELECT   'ZIP_TOO_MANY_FILES' as [Rulename]
+							,'F' as [Severity]
+							,'Zip folder must contain an XML file' as [Message]
+				UNION ALL 
+					SELECT   'ZIP_CORRUPT' as [Rulename]
+							,'F' as [Severity]
+							,'Zip folder is corrupt or invalid' as [Message]
+				UNION ALL 
+					SELECT   'Schema' AS [Rulename]
+							,'F' AS [Severity]
+							,'The XML is not well formed' as [Message]
+			  )
+			  AS Source 
+		    ON Target.[Rulename] = Source.[Rulename]
+			WHEN MATCHED 
+				AND EXISTS 
+					(	SELECT 
+							 Target.[Severity]
+							,Target.[Message]
+					EXCEPT 
+						SELECT 
+							 Source.[Severity]
+							,Source.[Message]
+					)
+		  THEN
+			UPDATE SET   [Severity] = Source.[Severity]
+						,[Message] = Source.[Message]
+		WHEN NOT MATCHED BY TARGET THEN
+		INSERT (     [Rulename]
+					,[Severity]
+					,[Message]
+					)
+			VALUES ( Source.[Rulename]
+					,Source.[Severity]
+					,Source.[Message]
+				  )
+		WHEN NOT MATCHED BY SOURCE THEN DELETE
+		;
+
+		
+		RETURN 0;
+
+	END TRY
+-- 
+-------------------------------------------------------------------------------------- 
+-- Handle any problems
+-------------------------------------------------------------------------------------- 
+-- 
+	BEGIN CATCH
+
+		DECLARE   @ErrorMessage		NVARCHAR(4000)
+				, @ErrorSeverity	INT 
+				, @ErrorState		INT
+				, @ErrorNumber		INT
+						
+		SELECT	  @ErrorNumber		= ERROR_NUMBER()
+				, @ErrorMessage		= 'Error in :' + ISNULL(OBJECT_NAME(@@PROCID),'') + ' - Error was :' + ERROR_MESSAGE()
+				, @ErrorSeverity	= ERROR_SEVERITY()
+				, @ErrorState		= ERROR_STATE();
+	
+		RAISERROR (
+					  @ErrorMessage		-- Message text.
+					, @ErrorSeverity	-- Severity.
+					, @ErrorState		-- State.
+				  );
+			  
+		RETURN @ErrorNumber;
+
+	END CATCH
+-- 
+-------------------------------------------------------------------------------------- 
+-- All done
+-------------------------------------------------------------------------------------- 
+-- 
+END
---
+++ src/ESFA.DC.Data.ILR.ValidationErrors.Database/Staging/Tables/ModifiedRules.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ModifiedRules]
+(
+	[Rulename] NVARCHAR(50) NOT NULL PRIMARY KEY, 
+	[Severity] NVARCHAR(1),
+    [Message] NVARCHAR(2000) NULL
+)
